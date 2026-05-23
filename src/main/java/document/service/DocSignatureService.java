5474ab2d71f6a01e2631de709214bf0501eaa1a8
mminjukim <mminju.km@gmail.com>
2026-05-23 22:02:20 +0900

    Fix/#9: 문서 전자서명 구현 로직 수정

--- src/main/java/document/service/DocSignatureService.java
+++ src/main/java/document/service/DocSignatureService.java
@@ -2,18 +2,13 @@ package main.java.document.service;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-
-import javax.crypto.Cipher;
+import java.security.Signature;
 
 public class DocSignatureService {
 
-	private static final String HASH_ALGORITHM = "SHA-256";
-	private static final String TRANSFORMATION = "RSA/ECB/PKCS1Padding";
 	private static final int BUFFER_SIZE = 1024; // 1KB
+	private static final String ALGORITHM = "SHA256withRSA";
 
 
 	/**
@@ -24,39 +19,23 @@ public class DocSignatureService {
 	 * @return 문서 전자서명 값
 	 */
 	public byte[] createSignature(String filePath, PrivateKey privateKey) {
-		byte[] hash = generateFileHash(filePath);
 		try {
-			Cipher cipher = Cipher.getInstance(TRANSFORMATION);
-			cipher.init(Cipher.ENCRYPT_MODE, privateKey);
-			return cipher.doFinal(hash);
-		} catch (Exception e) {
-			throw new IllegalStateException("문서의 전자서명 생성 중 오류가 발생했습니다.");
-		}  
-	}
-
-	/**
-	 * 파일 읽고 SHA-256 해시 생성
-	 * 
-	 * @param filePath 파일 경로
-	 * @return 생성된 해시 값 byte[]
-	 */
-	private byte[] generateFileHash(String filePath) {
-		try {
-			MessageDigest md = MessageDigest.getInstance(HASH_ALGORITHM);
+			Signature signature = Signature.getInstance(ALGORITHM);
+			signature.initSign(privateKey);
 
 			try (FileInputStream fis = new FileInputStream(filePath);
 					BufferedInputStream bis = new BufferedInputStream(fis)) {
 				byte[] buffer = new byte[BUFFER_SIZE];
 				int bytesRead;
 				while ((bytesRead = bis.read(buffer)) != -1) {
-					md.update(buffer, 0, bytesRead);
+					signature.update(buffer, 0, bytesRead);
 				}
 			}
-			return md.digest();
-		} catch (NoSuchAlgorithmException e) {
-			throw new IllegalStateException("문서의 해시 생성 중 오류가 발생했습니다.");
-		} catch (IOException e) {
-			throw new IllegalStateException("파일을 읽는 중 오류가 발생했습니다.");
+
+			return signature.sign();
+
+		} catch (Exception e) {
+			throw new IllegalStateException("문서의 전자서명 생성 중 오류가 발생했습니다.");
 		}
 	}
 }
