5f9052ed142ac5ce5ebc624f0c967d53ef15b2d4
prksih <psh031117@gmail.com>
2026-05-29 16:19:37 +0900

    Fix/#19: 문서 목록 비어있을 경우 예외 처리 추가 (문서열람, 로그조회 시)

--- src/main/java/document/controller/DocController.java
+++ src/main/java/document/controller/DocController.java
@@ -71,10 +71,14 @@ public class DocController {
 			System.out.println("[알림] 전체 문서 목록을 조회합니다...");
 
 			List<DocumentSummaryDto> documents = docService.getDocumentList();
+			if (documents.isEmpty()) {
+		        throw new IllegalStateException("등록된 문서가 없습니다.");
+			}
 			System.out.println("\n::: 문서 목록 :::");
 			for (DocumentSummaryDto doc : documents) {
 				System.out.println("[" + doc.getId() + "] " + doc.getTitle());
 			}
+			
 
 			System.out.print("\n열람할 문서의 번호를 입력하세요: ");
 			String docInput = scanner.nextLine();
--- src/main/java/log/controller/ReadLogController.java
+++ src/main/java/log/controller/ReadLogController.java
@@ -29,6 +29,9 @@ public class ReadLogController {
 			//전체 문서 목록 출력
 			System.out.println("[알림] 전체 문서 목록을 출력합니다...");
 			List<DocumentSummaryDto> documents = docService.getDocumentList();
+			if (documents.isEmpty()) {
+		        throw new IllegalStateException("등록된 문서가 없습니다.");
+			}
 			System.out.println("\n::: 문서 목록 :::");
 			for (DocumentSummaryDto doc : documents) {
 				System.out.println("[" + doc.getId() + "] " + doc.getTitle());
